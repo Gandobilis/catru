61c1c7ee43b76fb6d799b4427df07d4f8cb6e023
Gandobilis <lashadeveloper@gmail.com>
2024-10-19 15:59:04 +0400

    კონფიგურაციაში ფერების ცვლადები დაემატა.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        nino: ['nino', 'sans-serif'],  // Adding your custom font 'Nino'
-        bpg: ['bpg', 'sans-serif'],
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                mtavruli: ['mtavruli', 'sans-serif'],
+                mrglovani: ['mrglovani', 'sans-serif'],
+            },
+        },
+        colors: {
+            'primary-blue': '#1C5285',
+            'stroke-grey': '#D0D5DD',
+            'primary-button-hover': '#A39161',
+            'secondary-button-default': '#ED1C24',
+            'secondary-button-hover': '#BF161D',
+            'placeholder-grey': '#98A2B3',
+            'custom-green': '#117554'
+        }
     },
-  },
-  plugins: [
-    require('daisyui'),
-  ],
+    plugins: [
+        require('daisyui'),
+    ],
 }
