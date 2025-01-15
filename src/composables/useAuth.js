8677346e9b5d288263e5de40f6dd4b42d74a2bc9
gandobilis <lashadeveloper@gmail.com>
2025-01-16 00:13:11 +0400

    useAuth improved.

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,53 +1,44 @@
 import {ref} from "vue";
-import axios from "axios";
 import {useRouter} from "vue-router";
-import cookies from "vue-cookies"
 import axiosInstance from "../interceptors/axios/index.js";
 
 export default function useAuth() {
-    const user = ref({
-        username: undefined,
-        password: undefined,
-    });
-    const authError = ref();
     const router = useRouter();
+
+    const user = ref({
+        username: null,
+        password: null,
+    });
+
+    const authError = ref();
 
     const login = async () => {
         authError.value = null;
 
-        if (!user.value.username || !user.value.password) {
+        if (!user.value.username.trim() || !user.value.password.trim()) {
             authError.value = "გთხოვთ შეავსოთ მონაცემები.";
             return;
         }
 
-        // ავტორიზაციის რექვესთი
+        try {
+            await axiosInstance.post(`login`, {
+                username: user.value.username,
+                password: user.value.password,
+            });
 
-        const response = {
-            message: "Allow",
-            user_id: 1,
-        };
-
-        cookies.set("user_id", response.user_id);
-
-        if (response.message === "Allow") {
-            try {
-                await axiosInstance.post(`login`, {
-                    username: user.value.username,
-                    user_id: response.user_id
-                });
-
-                await router.push("/");
-            } catch (e) {
+            await router.push("/");
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                authError.value = error.response.data.message;
+            } else {
                 authError.value = "სერვერზე დაფიქსირდა შეცდომა.";
             }
-        } else if (response.message === "Reject") {
-            authError.value = "მომხმარებლის მონაცემები არასწორია.";
-        } else {
-            authError.value = "სერვერზე დაფიქსირდა შეცდომა.";
         }
     };
 
     return {
-        user, authError, login
+        user,
+        authError,
+        login,
     };
 };
