282aea14ecce511387a11a9af6fbfb78c2e87fe8
gandobilis <lashadeveloper@gmail.com>
2025-01-16 00:28:47 +0400

    Login.vue formatting added.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,9 +9,11 @@ const {user, authError, login} = useAuth();
   <div class="flex flex-col w-full gap-12 font-mtavruli ">
     <div class="flex flex-col gap-10 items-center text-center">
       <p class=" text-primary-blue  md:text-3xl text-2xl font-mtavruli">სისტემაში შესვლა</p>
+
       <div class="md:w-[50%] w-full font-mrglovani flex flex-col gap-10">
         <div class="flex flex-col  text-start gap-3">
           <p class="md:text-xl">მომხმარებელი</p>
+
           <input
               required
               v-model="user.username"
@@ -19,6 +21,7 @@ const {user, authError, login} = useAuth();
         </div>
         <div class="flex flex-col  text-start gap-3">
           <p class="md:text-xl">პაროლი</p>
+
           <input
               required
               v-model="user.password"
