9f25842e8a43f3b8e0665d5e7e57772e1a815f8b
gandobilis <lashadeveloper@gmail.com>
2025-01-16 01:04:24 +0400

    dynamic full name and id number binding logic added.

--- src/views/Verify.vue
+++ src/views/Verify.vue
@@ -1,28 +1,28 @@
 <script setup>
-import { watch, ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import useUser from "../composables/useUser.js";
+import {ref} from "vue";
+import {useRoute, useRouter} from "vue-router";
+import useUser from "/src/composables/useUser.js";
 
 const route = useRoute();
-const router = useRouter();  // Initialize router
+const router = useRouter();
 const uuid = route.params.uuid;
 const pageLoaded = ref(false);
-
-const { isChecked, acceptForm, visitLink, failed, visitLinkResponse, smsError } = useUser();
+const {isChecked, acceptForm, visitLink, failed, visitLinkResponse, smsError} = useUser();
 
 const checkFailed = async () => {
   await visitLink(uuid);
   if (failed.value) {
-    await router.push('/not-found');  // Redirect to not-found page
+    await router.push('/not-found');
   } else {
     pageLoaded.value = true;
   }
 };
 
 checkFailed();
+
+// const consentText = import.meta.env.VITE_CONSENT_TEXT.replace('FULL_NAME', '<span class="text-primary-blue font-mrglovani">FULL NAME</span>')
+//    .replace('ID_NUMBER', '<span class="text-primary-blue font-mrglovani">ID NUMBER</span>');
 </script>
-
-
 
 <template>
   <div v-if="pageLoaded" class="flex flex-col w-full gap-12 font-mtavruli">
@@ -35,18 +35,24 @@ checkFailed();
         <hr class="text-placeholder-grey"/>
 
         <p class="md:text-lg text-sm font-mrglovani">
-          &nbsp;მე, <span class="text-primary-blue font-mrglovani">{{visitLinkResponse?.fullName}} </span> (პირადი ნომერი <span class="text-primary-blue font-mrglovani">{{visitLinkResponse?.idNum}}</span>),
-          თანხმობას ვაცხადებ, რომ სს”ბანკი ქართუ”-მ ერთჯერადად გამოითხოვოს და მიიღოს საქართველოს ფინანსთა სამინისტროს შემოსავლების სამსახურის მონაცემთა ერთიან ელექტრონულ ბაზაში
-          ჩემზე არსებული ნებისმიერი სახის ინფორმაცია, რომელიც დაკავშირებულია ჩემს შემოსავლებთან. ჩემთვის ცნობილია და თანხმობას ვაცხადებ, რომ მონაცემთა
-          დამუშავების მიზანია: ჩემი გადახდისუნარიანობის შეფასება სესხის გაცემამდე. მე თანახმა ვარ ბანკმა მიღებული ინფორმაცია დაამუშავოს, შეინახოს და გამოიყენოს
+          &nbsp;მე, <span class="text-primary-blue font-mrglovani">{{ visitLinkResponse?.fullName }} </span> (პირადი
+          ნომერი <span class="text-primary-blue font-mrglovani">{{ visitLinkResponse?.idNum }}</span>),
+          თანხმობას ვაცხადებ, რომ სს”ბანკი ქართუ”-მ ერთჯერადად გამოითხოვოს და მიიღოს საქართველოს ფინანსთა სამინისტროს
+          შემოსავლების სამსახურის მონაცემთა ერთიან ელექტრონულ ბაზაში
+          ჩემზე არსებული ნებისმიერი სახის ინფორმაცია, რომელიც დაკავშირებულია ჩემს შემოსავლებთან. ჩემთვის ცნობილია და
+          თანხმობას ვაცხადებ, რომ მონაცემთა
+          დამუშავების მიზანია: ჩემი გადახდისუნარიანობის შეფასება სესხის გაცემამდე. მე თანახმა ვარ ბანკმა მიღებული
+          ინფორმაცია დაამუშავოს, შეინახოს და გამოიყენოს
           ჩემტან სახელშეკრულებო ურთიერთობის დამყარების ან მასზე უარის თქმის შემთხვევაში.
-          <br />&nbsp;მოცემული თანხმობის მოქმედების ვადაა 1 (ერთი) თვე მისი გაცემიდან.
+          <br/>&nbsp;მოცემული თანხმობის მოქმედების ვადაა 1 (ერთი) თვე მისი გაცემიდან.
         </p>
+
         <hr class="text-placeholder-grey"/>
 
         <div class="flex gap-1.5 items-center">
           <input type="checkbox" v-model="isChecked"
                  class="md:w-[20px] md:h-[20px] w-14px h-14px appearance-none border rounded custom-checkbox"/>
+
           <p class="md:text-lg text-sm">ვეთანხმები</p>
         </div>
 
